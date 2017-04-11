423caad787c753d825383e879bd16d7ca5acad7d
bldm95 <bldm95@mail.ru>
2017-04-11 16:06:36 +0300

    Add requirements

--- home/tests.py
+++ home/tests.py
@@ -3,25 +3,33 @@ from home.models import News
 import factory
 
 
+#
 
-#
-#
-class HomePageTest(TestCase):
-    #
-    def test_homepage_available(self):
-        c = Client()
-        response = c.get('/')
-        self.assertEquals(response.status_code, 200)
 
 
 class NewsTestCase(TestCase):
     def setUp(self):
-        News.objects.create(title='Test', text='Test Test', image='/',
-                            created_date='2016-10-05 09:41:48.636448', published_date='2016-11-26 20:45:00.000000')
-        News.objects.create(title='Test2', text='When you run your tests,', image='/3',
-                            created_date='2016-10-04 10:45:48.636448', published_date='2016-12-26 21:30:00.000000')
+        # self.news = []
+        ##    new_news = News.objects.create(title="Test %d %i", text="Test Test %d %i", image='/',
+        ##                          created_date='2016-10-05 09:41:48.636448',
+        ##                           published_date='2016-11-26 20:45:00.000000')
+        #   self.news.append(new_news)
+        self.one_new = News.objects.create(title="Test s", text="Test Test s", image='/',
+                                           created_date='2016-10-05 09:41:48.636448',
+                                           published_date='2016-11-26 20:45:00.000000')
+        #
 
-  #  def test_news(self):
-      #  news1 = News.objects.get(title='Test')
+    def test_homepage_contains_posts(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.one_new.title, "Test s")
+        #self.assertIn(self.one_new.title, response.content_type)
+        # self.assertIn(self.news[2].title, response.content)
 
-    #    self.assertEqual(news1)
+
+
+        #  def test_news(self):
+        #  news1 = News.objects.get(title='Test')
+
+        #    self.assertEqual(news1)
