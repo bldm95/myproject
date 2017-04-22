c0a509e5955194df26c904cb4b295dbbf88f010a
bldm95 <bldm95@mail.ru>
2017-04-22 08:12:05 +0300

    Del doctest and comments

--- home/__init__.py
+++
@@ -1,8 +0,0 @@
-
-'''
-import home.mydoctest
-from home import mydoctest
-
-__test__ = {'Doctest': mydoctest}
-
-'''
--- home/tests.py
+++ home/tests.py
@@ -1,18 +1,28 @@
-from django.template import response
-from django.test import TestCase, Client
-from home.models import News
-import factory
+from datetime import datetime
 
+from django.test import Client
 from django.test import TestCase
 from home.models import News
+from home import models
 from django.utils import timezone
 from django.core.urlresolvers import reverse
+import unittest
+from tastypie.test import ResourceTestCase
+
+
+#
+class HomePageTest(TestCase):
+    def test_homepage_available(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEquals(response.status_code, 200)
+
 
 # models test
 class NewsTest(TestCase):
     def create_news(self, title="Test s", text="Test Test s", image='/',
-                    created_date='2016-10-05 09:41:48.636448',
-                    published_date='2016-11-26 20:45:00.000000'):
+                    created_date=timezone.now(),
+                    published_date=timezone.now()):
         return News.objects.create(title=title, text=text, image=image, created_date=created_date,
                                    published_date=published_date)
 
@@ -21,44 +31,14 @@ class NewsTest(TestCase):
         self.assertTrue(isinstance(n, News))
         self.assertEqual(n.__unicode__(), n.title)
 
-#views
+    # views
 
     def test_news_list_view(self):
         n = self.create_news()
         url = reverse('news_detail', args=[n.id])
         resp = self.client.get(url)
-        #self.assertTemplateUsed(response, "home/news_detail.html")
+        # self.assertTemplateUsed(response, "home/news_detail.html")
         self.assertEqual(resp.status_code, 200)
         self.assertIn(n.title, resp.content.decode('UTF-8'))
 
-#
 
-'''
-
-class NewsTestCase(TestCase):
-    def setUp(self):
-        # self.news = []
-        ##    new_news = News.objects.create(title="Test %d %i", text="Test Test %d %i", image='/',
-        ##                          created_date='2016-10-05 09:41:48.636448',
-        ##                           published_date='2016-11-26 20:45:00.000000')
-        #   self.news.append(new_news)
-        self.one_new = News.objects.create(title="Test s", text="Test Test s", image='/',
-                                           created_date='2016-10-05 09:41:48.636448',
-                                           published_date='2016-11-26 20:45:00.000000')
-        #
-
-    def test_homepage_contains_posts(self):
-        c = Client()
-        response = c.get('/')
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(self.one_new.title, "Test s")
-        #self.assertIn(self.one_new.title, response.content_type)
-        # self.assertIn(self.news[2].title, response.content)
-
-
-
-        #  def test_news(self):
-        #  news1 = News.objects.get(title='Test')
-
-        #    self.assertEqual(news1)
-'''
